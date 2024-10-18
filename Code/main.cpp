ba3f4e3357f486a3a27b3296ae85fa6bbb2fc4a5
Katelyne Aguilar <111928600+katelynea@users.noreply.github.com>
2024-10-18 18:24:03 -0500

    ++

--- Code/main.cpp
+++ Code/main.cpp
@@ -79,3 +79,11 @@ int main(int argc, char** argv) {
     profiler = nullptr;
     return 0;
 }
+
+// compile: 
+// 	clear 
+// 	clang++ -g -std=c++14 ./Code/*.cpp -o output
+// 	./output
+// 	@echo "Running Python visualization..."
+// 	@conda run -n intromlenv python ./Code/visualize_detailed_profiling.py
+// 	@conda run -n intromlenv python ./Code/visualize_profilerStats.py
